96e912fcce7f6fe3a1b456cf2d05ea073dd7f1cf
Gene Goykhman <gene@indigo1.com>
2013-04-11 01:38:11 -0400

    0-day and 30-day JSON-formatted reports for Client, Project, and Category

--- Main.Designer.vb
+++ Main.Designer.vb
@@ -22,7 +22,6 @@ Partial Class frmMain
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-		Me.Label1 = New System.Windows.Forms.Label
 		Me.GroupBox1 = New System.Windows.Forms.GroupBox
 		Me.cmdTestConnection = New System.Windows.Forms.Button
 		Me.Label6 = New System.Windows.Forms.Label
@@ -31,17 +30,21 @@ Partial Class frmMain
 		Me.txtPassword = New System.Windows.Forms.TextBox
 		Me.txtUser = New System.Windows.Forms.TextBox
 		Me.txtURL = New System.Windows.Forms.TextBox
+		Me.frameData = New System.Windows.Forms.GroupBox
+		Me.chkTodayByCategory = New System.Windows.Forms.CheckBox
+		Me.chkTodayByProject = New System.Windows.Forms.CheckBox
+		Me.chkTodayByClient = New System.Windows.Forms.CheckBox
+		Me.GroupBox2 = New System.Windows.Forms.GroupBox
+		Me.Label1 = New System.Windows.Forms.Label
+		Me.txtDestinationFolder = New System.Windows.Forms.TextBox
+		Me.cmdUpdateNow = New System.Windows.Forms.Button
+		Me.chk30DayByCategory = New System.Windows.Forms.CheckBox
+		Me.chk30DayByProject = New System.Windows.Forms.CheckBox
+		Me.chk30DayByClient = New System.Windows.Forms.CheckBox
 		Me.GroupBox1.SuspendLayout()
+		Me.frameData.SuspendLayout()
+		Me.GroupBox2.SuspendLayout()
 		Me.SuspendLayout()
-		'
-		'Label1
-		'
-		Me.Label1.AutoSize = True
-		Me.Label1.Location = New System.Drawing.Point(12, 19)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(146, 13)
-		Me.Label1.TabIndex = 2
-		Me.Label1.Text = "TimeTiger StatusBoard Query"
 		'
 		'GroupBox1
 		'
@@ -52,7 +55,7 @@ Partial Class frmMain
 		Me.GroupBox1.Controls.Add(Me.txtPassword)
 		Me.GroupBox1.Controls.Add(Me.txtUser)
 		Me.GroupBox1.Controls.Add(Me.txtURL)
-		Me.GroupBox1.Location = New System.Drawing.Point(15, 47)
+		Me.GroupBox1.Location = New System.Drawing.Point(13, 12)
 		Me.GroupBox1.Name = "GroupBox1"
 		Me.GroupBox1.Size = New System.Drawing.Size(473, 116)
 		Me.GroupBox1.TabIndex = 0
@@ -119,23 +122,139 @@ Partial Class frmMain
 		Me.txtURL.TabIndex = 1
 		Me.txtURL.Text = "https://www.timetiger.com/online/sample"
 		'
+		'frameData
+		'
+		Me.frameData.Controls.Add(Me.chk30DayByCategory)
+		Me.frameData.Controls.Add(Me.chk30DayByProject)
+		Me.frameData.Controls.Add(Me.chk30DayByClient)
+		Me.frameData.Controls.Add(Me.chkTodayByCategory)
+		Me.frameData.Controls.Add(Me.chkTodayByProject)
+		Me.frameData.Controls.Add(Me.chkTodayByClient)
+		Me.frameData.Location = New System.Drawing.Point(14, 198)
+		Me.frameData.Name = "frameData"
+		Me.frameData.Size = New System.Drawing.Size(473, 95)
+		Me.frameData.TabIndex = 1
+		Me.frameData.TabStop = False
+		Me.frameData.Text = "Data"
+		'
+		'chkTodayByCategory
+		'
+		Me.chkTodayByCategory.AutoSize = True
+		Me.chkTodayByCategory.Location = New System.Drawing.Point(22, 66)
+		Me.chkTodayByCategory.Name = "chkTodayByCategory"
+		Me.chkTodayByCategory.Size = New System.Drawing.Size(144, 17)
+		Me.chkTodayByCategory.TabIndex = 2
+		Me.chkTodayByCategory.Text = "Today's time by Category"
+		Me.chkTodayByCategory.UseVisualStyleBackColor = True
+		'
+		'chkTodayByProject
+		'
+		Me.chkTodayByProject.AutoSize = True
+		Me.chkTodayByProject.Location = New System.Drawing.Point(22, 43)
+		Me.chkTodayByProject.Name = "chkTodayByProject"
+		Me.chkTodayByProject.Size = New System.Drawing.Size(135, 17)
+		Me.chkTodayByProject.TabIndex = 1
+		Me.chkTodayByProject.Text = "Today's time by Project"
+		Me.chkTodayByProject.UseVisualStyleBackColor = True
+		'
+		'chkTodayByClient
+		'
+		Me.chkTodayByClient.AutoSize = True
+		Me.chkTodayByClient.Location = New System.Drawing.Point(22, 20)
+		Me.chkTodayByClient.Name = "chkTodayByClient"
+		Me.chkTodayByClient.Size = New System.Drawing.Size(128, 17)
+		Me.chkTodayByClient.TabIndex = 0
+		Me.chkTodayByClient.Text = "Today's time by Client"
+		Me.chkTodayByClient.UseVisualStyleBackColor = True
+		'
+		'GroupBox2
+		'
+		Me.GroupBox2.Controls.Add(Me.Label1)
+		Me.GroupBox2.Controls.Add(Me.txtDestinationFolder)
+		Me.GroupBox2.Location = New System.Drawing.Point(14, 134)
+		Me.GroupBox2.Name = "GroupBox2"
+		Me.GroupBox2.Size = New System.Drawing.Size(472, 58)
+		Me.GroupBox2.TabIndex = 2
+		Me.GroupBox2.TabStop = False
+		Me.GroupBox2.Text = "Destination"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(6, 22)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(36, 13)
+		Me.Label1.TabIndex = 8
+		Me.Label1.Text = "Folder"
+		'
+		'txtDestinationFolder
+		'
+		Me.txtDestinationFolder.Location = New System.Drawing.Point(65, 19)
+		Me.txtDestinationFolder.Name = "txtDestinationFolder"
+		Me.txtDestinationFolder.Size = New System.Drawing.Size(390, 20)
+		Me.txtDestinationFolder.TabIndex = 7
+		Me.txtDestinationFolder.Text = "Z:\Public"
+		'
+		'cmdUpdateNow
+		'
+		Me.cmdUpdateNow.Location = New System.Drawing.Point(380, 304)
+		Me.cmdUpdateNow.Name = "cmdUpdateNow"
+		Me.cmdUpdateNow.Size = New System.Drawing.Size(106, 50)
+		Me.cmdUpdateNow.TabIndex = 3
+		Me.cmdUpdateNow.Text = "Update Now"
+		Me.cmdUpdateNow.UseVisualStyleBackColor = True
+		'
+		'chk30DayByCategory
+		'
+		Me.chk30DayByCategory.AutoSize = True
+		Me.chk30DayByCategory.Location = New System.Drawing.Point(192, 66)
+		Me.chk30DayByCategory.Name = "chk30DayByCategory"
+		Me.chk30DayByCategory.Size = New System.Drawing.Size(139, 17)
+		Me.chk30DayByCategory.TabIndex = 5
+		Me.chk30DayByCategory.Text = "30-day time by Category"
+		Me.chk30DayByCategory.UseVisualStyleBackColor = True
+		'
+		'chk30DayByProject
+		'
+		Me.chk30DayByProject.AutoSize = True
+		Me.chk30DayByProject.Location = New System.Drawing.Point(192, 43)
+		Me.chk30DayByProject.Name = "chk30DayByProject"
+		Me.chk30DayByProject.Size = New System.Drawing.Size(130, 17)
+		Me.chk30DayByProject.TabIndex = 4
+		Me.chk30DayByProject.Text = "30-day time by Project"
+		Me.chk30DayByProject.UseVisualStyleBackColor = True
+		'
+		'chk30DayByClient
+		'
+		Me.chk30DayByClient.AutoSize = True
+		Me.chk30DayByClient.Location = New System.Drawing.Point(192, 20)
+		Me.chk30DayByClient.Name = "chk30DayByClient"
+		Me.chk30DayByClient.Size = New System.Drawing.Size(123, 17)
+		Me.chk30DayByClient.TabIndex = 3
+		Me.chk30DayByClient.Text = "30-day time by Client"
+		Me.chk30DayByClient.UseVisualStyleBackColor = True
+		'
 		'frmMain
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(498, 181)
+		Me.ClientSize = New System.Drawing.Size(498, 366)
+		Me.Controls.Add(Me.cmdUpdateNow)
+		Me.Controls.Add(Me.GroupBox2)
+		Me.Controls.Add(Me.frameData)
 		Me.Controls.Add(Me.GroupBox1)
-		Me.Controls.Add(Me.Label1)
 		Me.Name = "frmMain"
 		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-		Me.Text = "TimeTiger StatusBoard tool"
+		Me.Text = "TimeTiger StatusBoard"
 		Me.GroupBox1.ResumeLayout(False)
 		Me.GroupBox1.PerformLayout()
+		Me.frameData.ResumeLayout(False)
+		Me.frameData.PerformLayout()
+		Me.GroupBox2.ResumeLayout(False)
+		Me.GroupBox2.PerformLayout()
 		Me.ResumeLayout(False)
-		Me.PerformLayout()
 
 	End Sub
-	Friend WithEvents Label1 As System.Windows.Forms.Label
 	Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
 	Friend WithEvents txtUser As System.Windows.Forms.TextBox
 	Friend WithEvents txtURL As System.Windows.Forms.TextBox
@@ -144,5 +263,16 @@ Partial Class frmMain
 	Friend WithEvents Label4 As System.Windows.Forms.Label
 	Friend WithEvents txtPassword As System.Windows.Forms.TextBox
 	Friend WithEvents cmdTestConnection As System.Windows.Forms.Button
+	Friend WithEvents frameData As System.Windows.Forms.GroupBox
+	Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+	Friend WithEvents Label1 As System.Windows.Forms.Label
+	Friend WithEvents txtDestinationFolder As System.Windows.Forms.TextBox
+	Friend WithEvents cmdUpdateNow As System.Windows.Forms.Button
+	Friend WithEvents chkTodayByCategory As System.Windows.Forms.CheckBox
+	Friend WithEvents chkTodayByProject As System.Windows.Forms.CheckBox
+	Friend WithEvents chkTodayByClient As System.Windows.Forms.CheckBox
+	Friend WithEvents chk30DayByCategory As System.Windows.Forms.CheckBox
+	Friend WithEvents chk30DayByProject As System.Windows.Forms.CheckBox
+	Friend WithEvents chk30DayByClient As System.Windows.Forms.CheckBox
 
 End Class
--- Main.vb
+++ Main.vb
@@ -1,9 +1,9 @@
 Imports Microsoft.VisualBasic.FileIO
 Imports Microsoft.VisualBasic.FileIO.TextFieldParser
+Imports System.IO
+Imports System.Collections
 
 Public Class frmMain
-	Private m_entityTypes As Dictionary(Of String, ttsystem.EntityType)
-
 	Private Sub cmdTestConnection_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdTestConnection.Click
 		Dim tt As New ttsystem.ttsystem
 		Try
@@ -25,5 +25,151 @@ Public Class frmMain
 			Throw New Exception(response.Message)
 		End If
 		ConnectAndGetSessionID = sessionID
+	End Function
+
+	Private Sub cmdUpdateNow_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdUpdateNow.Click
+		Dim tt As New ttsystem.ttsystem
+		Try
+			Cursor.Current = Cursors.WaitCursor
+			Dim sessionID As String = ConnectAndGetSessionID(tt)
+			GenerateReports(tt, sessionID)
+			tt.Goodbye(sessionID)
+		Catch ex As Exception
+			MessageBox.Show(ex.Message, "Test connection", MessageBoxButtons.OK, MessageBoxIcon.Error)
+		End Try
+	End Sub
+
+	Private Sub GenerateReports(ByRef tt As ttsystem.ttsystem, ByRef sessionID As String)
+		If (chkTodayByClient.Checked = True) Then
+			GenerateTimeReportWithDateRange(tt, sessionID, Date.Today, Date.Today, "Client")
+		End If
+		If (chkTodayByProject.Checked = True) Then
+			GenerateTimeReportWithDateRange(tt, sessionID, Date.Today, Date.Today, "Project")
+		End If
+		If (chkTodayByCategory.Checked = True) Then
+			GenerateTimeReportWithDateRange(tt, sessionID, Date.Today, Date.Today, "Category")
+		End If
+		If (chk30DayByClient.Checked = True) Then
+			GenerateTimeReportWithDateRange(tt, sessionID, Date.Today.AddDays(-30), Date.Today, "Client")
+		End If
+		If (chk30DayByProject.Checked = True) Then
+			GenerateTimeReportWithDateRange(tt, sessionID, Date.Today.AddDays(-30), Date.Today, "Project")
+		End If
+		If (chk30DayByCategory.Checked = True) Then
+			GenerateTimeReportWithDateRange(tt, sessionID, Date.Today.AddDays(-30), Date.Today, "Category")
+		End If
+		MsgBox("Done", MsgBoxStyle.Information)
+	End Sub
+
+	Private Sub GenerateTimeReportWithDateRange(ByRef tt As ttsystem.ttsystem, ByRef sessionID As String, ByRef dFrom As Date, ByRef dTo As Date, ByRef entityType As String)
+		Dim entities As ttsystem.GetAllEntitiesOutput
+		entities = tt.GetAllEntities(sessionID, "0", "0")
+		Dim entityLookup As New Dictionary(Of String, String)
+		BuildEntityLookupWithEntityType(entities.EntityNodes, entityType, entityLookup)
+
+		Dim sFrom As String = dFrom.ToString("yyyyMMdd")
+		Dim sTo As String = dTo.ToString("yyyyMMdd")
+
+		' Get a good name for the report
+		Dim dateDiff As TimeSpan = dTo - dFrom
+		Dim sDays = (dateDiff.Days).ToString() + "-day"
+
+		Dim listResults As New Dictionary(Of String, String)
+		GenerateTimeReport(tt, sessionID, sFrom, sTo, entityLookup, listResults)
+		SaveResultsAsJSON(listResults, txtDestinationFolder.Text + "\" + sDays + "-" + entityType.ToLower() + ".json", IIf(sDays = "0-Day", "Today's", sDays) + " time by " + entityType)
+	End Sub
+
+	Private Sub SaveResultsAsJSON(ByRef listResults As Dictionary(Of String, String), ByRef outputFileName As String, ByRef reportTitle As String)
+		Using outFile As New StreamWriter(outputFileName)
+			outFile.Write("{ ""graph"" : { ""title"" : """ + reportTitle)
+			outFile.Write(""", ""datasequences"" : [ { ""title"" : ""Hours"", ""datapoints"" : [")
+			For Each desiredEntityName As String In listResults.Keys
+				Dim sSecs As String = listResults(desiredEntityName)
+				Dim sDuration As String = (Convert.ToDouble(sSecs) / 3600).ToString(".00")	 ' 2 decimal places
+				outFile.Write("{ ""title"" : """ + PrepEntityName(desiredEntityName) + """, ""value"" : " + sDuration + " }," + vbCrLf)
+			Next
+			outFile.Write("] } ] } }")
+		End Using
+	End Sub
+
+	Private Function PrepEntityName(ByVal original As String) As String
+		Dim work As String = original
+		If (work.Length > 10) Then
+			work = work.Substring(0, 9) + "..."
+		End If
+		work = work.Replace("""", "\x22")
+		Return work
+	End Function
+
+	Private Sub BuildEntityLookupWithEntityType(ByRef nodes As ttsystem.EntityNode(), ByRef entityType As String, ByRef entityLookup As Dictionary(Of String, String))
+		' Recurse through all entities in the system
+
+		For Each node As ttsystem.EntityNode In nodes
+			If (node.ChildNodes.Length > 0) Then
+				BuildEntityLookupWithEntityTypeRecurse(node.ChildNodes, entityType, entityLookup, "")
+			End If
+		Next
+	End Sub
+
+	Private Sub BuildEntityLookupWithEntityTypeRecurse(ByRef nodes As ttsystem.EntityNode(), ByRef entityType As String, ByRef entityLookup As Dictionary(Of String, String), ByVal rollupToEntityName As String)
+		For Each node As ttsystem.EntityNode In nodes
+			Dim rollupChildren As String = rollupToEntityName
+			If node.EntityTypeName = entityType And rollupChildren.Length = 0 Then
+				rollupChildren = node.EntityName
+			End If
+
+			If rollupChildren.Length > 0 And node.ChildNodes.Length = 0 Then	' Lowest level
+				entityLookup(node.UID) = rollupChildren
+			End If
+
+			' Recurse into children
+
+			If (node.ChildNodes.Length > 0) Then
+				BuildEntityLookupWithEntityTypeRecurse(node.ChildNodes, entityType, entityLookup, rollupChildren)
+			End If
+		Next
+	End Sub
+
+	Private Sub GenerateTimeReport(ByRef tt As ttsystem.ttsystem, ByRef sessionID As String, ByRef fromDate As String, ByRef toDate As String, ByRef entityLookup As Dictionary(Of String, String), ByRef listResults As Dictionary(Of String, String))
+		Dim logs As ttsystem.GetTimeLogsOutput
+		logs = tt.GetTimeLogs(sessionID, "", fromDate, toDate, "7", "0", "")
+		For Each log As ttsystem.TimeLog In logs.TimeLogs
+			AddLogToResults(log, entityLookup, listResults)
+		Next
+	End Sub
+
+	Private Sub AddLogToResults(ByRef log As ttsystem.TimeLog, ByRef entityLookup As Dictionary(Of String, String), ByRef listResults As Dictionary(Of String, String))
+		' Find the type-appropriate item to which this log belongs
+
+		Dim desiredEntityName As String = LookupLogInEntityList(log, entityLookup)
+		If (desiredEntityName.Length = 0) Then
+			' Couldn't find the entity, perhaps because we lack appropriate permissions?
+			Debug.Assert(False)
+			Exit Sub
+		End If
+
+		' See if we already have a results entry for it
+
+		Dim iSecs As Integer = 0
+		If listResults.ContainsKey(desiredEntityName) Then
+			iSecs = Convert.ToInt32(listResults(desiredEntityName))
+		End If
+
+		Dim iAdditionalSecs As Integer = Convert.ToInt32(log.Duration)
+		listResults(desiredEntityName) = (iSecs + iAdditionalSecs).ToString()
+	End Sub
+
+	Private Function LookupLogInEntityList(ByRef log As ttsystem.TimeLog, ByRef entityLookup As Dictionary(Of String, String)) As String
+		' Naively check every lowest level entity ID in the log's allocation.
+		' We could in theory optimize this by just checking the lowest-level types related
+		' to the entity type we're querying, but this is sufficiently performant as-is
+
+		For Each entry As ttsystem.AllocationEntry In log.Allocation
+			If entityLookup.ContainsKey(entry.EntityID) Then
+				Return entityLookup(entry.EntityID)
+			End If
+		Next
+		' Not found
+		Return ""
 	End Function
 End Class
